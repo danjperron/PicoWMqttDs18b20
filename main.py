9a179c759d96d814403a4a0fd8687d173d6080b4
Daniel Perron <danjperron@videotron.ca>
2022-07-11 16:42:43 -0400

    set correct script to run

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import machine
 led = machine.Pin('LED', machine.Pin.OUT)
 sw = machine.Pin(19,machine.Pin.IN,machine.Pin.PULL_UP)
 
+#on test to prevent to always blank flashing the pico
+#I used GPIO(19) to detect if I want to break the system instead of running the MQTT script
 if  sw.value():
-    import mqt4
+    import mqtt_ds18B20.py
 else:
     led.value(0)
     sys.exit()
